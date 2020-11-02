b03554d0563e8a0d14a57d292c576963e826d1e3
Jogi Nayak <jogi@Jogis-MacBook-Pro.local>
2020-11-02 12:55:31 +0530

    api url and site url change

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    API_URI:"https://centrestage-api.santoorstayyoung.com", 
-    SITE_URL:"",
+    API_URI:"https://centrestage-api-new.santoorstayyoung.com/", 
     MOBILE_BREAKPOINT: 768,
     TOKEN_VAR_NAME: 'dgdfzhbrgshjsetahegh',
     DONT_LOGIN_REDIRECT: ['admin'],
     NAV_HEIGHT:"52px",
-    SITE_URL:"https://centrestage2.santoorstayyoung.com",
+    SITE_URL:"https://santoor-frontend.vercel.app",
     ADMIN_PER_PAGE:50,
     TOTAL_POLL_COUNT:60,
     POLL_INTERVAL:30000
